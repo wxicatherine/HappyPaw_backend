c407296617e577a77c5dc78e1665b186b2e3939d
MarianaKlochnyk <mariana.klochnyk.pm.2023@lpnu.ua>
2025-02-18 20:40:40 +0200

    Right file. Add login page

---
+++ reciclica-app/src/app/pages/home/home.page.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.page.html',
+  styleUrls: ['./home.page.scss'],
+  imports: [
+    IonicModule
+  ]
+})
+export class HomePage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ reciclica-app/src/app/pages/login/login.page.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+  imports: [
+    IonicModule
+  ]
+})
+export class LoginPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
